40345866d3707cc31d2e356161a360c9903f46ae
Vardan Sargsyan <vardan@xamlab.com>
2019-08-08 19:24:02 +0400

    minor fixes

--- KoinJunitTest/koinjunittestviewmodels/src/main/java/com/example/koinjunittestviewmodels/main/implementation/MainViewModel.kt
+++ KoinJunitTest/koinjunittestviewmodels/src/main/java/com/example/koinjunittestviewmodels/main/implementation/MainViewModel.kt
@@ -1,11 +1,12 @@
 package com.example.koinjunittestviewmodels.main.implementation
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.koinjunittestviewmodels.base.ICommand
 import com.example.koinjunittestviewmodels.main.IMainViewModel
 
 
-class MainViewModel : IMainViewModel {
+class MainViewModel : IMainViewModel, ViewModel() {
 
     override var ParamOne: MutableLiveData<Int> = MutableLiveData(2)
 
--- KoinJunitTest/koinjunittestviewmodeltests/src/test/java/com/example/koinjunittestviewmodeltests/ExampleUnitTest.kt
+++ KoinJunitTest/koinjunittestviewmodeltests/src/test/java/com/example/koinjunittestviewmodeltests/ExampleUnitTest.kt
@@ -1,17 +1,45 @@
 package com.example.koinjunittestviewmodeltests
 
+import com.example.koinjunittestviewmodels.di.viewModelsModule
+import com.example.koinjunittestviewmodels.main.IMainViewModel
+import org.junit.After
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.test.KoinTest
+import org.koin.test.inject
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class ExampleUnitTest : KoinTest {
+
+    private val viewModel by inject<IMainViewModel>()
+
+    @Before
+    fun before() {
+        startKoin {
+            modules(viewModelsModule)
+        }
+    }
+
+    @After
+    fun after() {
+        stopKoin()
+    }
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+
+
+        viewModel.ParamOne.postValue(5)
+        viewModel.ParamTwo.postValue(5)
+        viewModel.AddCommand.execute()
+        assertEquals(10, viewModel.result)
     }
 }
